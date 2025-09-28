cf8e6092aeb20966e84a2f04247099364e51b15c
Austin Oligario <austinoligario@gmail.com>
2025-09-28 19:34:10 -0400

    remove onetap config

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,7 +9,6 @@ export const authClient = createAuthClient({
   plugins: [
     oneTapClient({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-      cancelOnTapOutside: false,
     }),
   ],
 });
