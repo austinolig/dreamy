e548eeaa00850e64e2cd80f5eef3db53c7df54e9
Austin Oligario <austinoligario@gmail.com>
2025-10-03 18:47:29 -0400

    switch model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: openrouter("x-ai/grok-4-fast:free"),
+    model: openrouter("openai/gpt-oss-20b:free"),
     messages: convertToModelMessages(messages),
   });
 
