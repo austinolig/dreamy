e6a055961da5e742b1122548da43ed090655091e
Austin Oligario <austinoligario@gmail.com>
2025-10-04 01:20:07 -0400

    added better ux while generating

--- src/components/dream-analysis.tsx
+++ src/components/dream-analysis.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-// import { Skeleton } from "@/components/ui/skeleton";
 import { Response } from "@/components/ai-elements/response";
 import { SparklesIcon } from "lucide-react";
 import { useChat } from "@ai-sdk/react";
@@ -41,30 +40,29 @@ export function DreamAnalysisSection({
     }
   }, [isNap, description, analysis, sendMessage, dreamLogId]);
 
+  const responseComplete = messages[messages.length - 1]?.parts.some(
+    (part) => part.type === "text" && part.state === "done"
+  );
+
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="flex items-center justify-between">
         <CardTitle className="flex items-center gap-2">
           <SparklesIcon className="size-5" />
           Dream Analysis
-          {/* {isStreaming && (
-            <span className="text-xs font-normal text-muted-foreground">
-              (Generating...)
-            </span>
-          )} */}
         </CardTitle>
+        {/* {!responseComplete && <LoaderCircle className="size-5 animate-spin" />} */}
       </CardHeader>
-      <CardContent className="space-y-4">
+      <CardContent
+        className={cn(
+          "border bg-background mx-6 p-6 rounded-xl prose prose-sm dark:prose-invert",
+          !responseComplete ? "animate-pulse" : ""
+        )}
+      >
         {messages.map(
           (message) =>
             message.role !== "user" && (
-              <div
-                key={message.id}
-                className={cn(
-                  "whitespace-pre-wrap p-4 rounded-lg",
-                  "bg-muted/80 text-muted-foreground"
-                )}
-              >
+              <div key={message.id} className="">
                 {message.parts.map((part, i) => {
                   switch (part.type) {
                     case "text":
@@ -78,19 +76,6 @@ export function DreamAnalysisSection({
               </div>
             )
         )}
-        {/* {analysis?.content ? (
-          <div className="prose prose-sm dark:prose-invert max-w-none">
-            <Response>{analysis.content}</Response>
-          </div>
-        ) : (
-          <div className="space-y-3">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-3/4" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-5/6" />
-          </div>
-        )} */}
       </CardContent>
     </Card>
   );
